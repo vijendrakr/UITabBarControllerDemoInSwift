7fd8ee7c1da9f9a16960bec67554b26e76735c0e
vijendra <yv.vijendra@gmail.com>
2019-03-26 02:48:30 +0530

    Update

--- UITabBarControllerDemoInSwift/TabBarViewController.swift
+++ UITabBarControllerDemoInSwift/TabBarViewController.swift
@@ -46,6 +46,9 @@ class TabBarViewController: UITabBarController {
         let unSelectedIcon:UIImage = UIImage(named: unselectedImage as String)!
         
         let tabBarItem = UITabBarItem.init(title: title, image: unSelectedIcon, selectedImage: selectedIcon)
+        
+        // tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0) [use this where there is no title for tabbar item to position the image perfectly]
+        
         tabBarItem.tag = tag
         return tabBarItem
     }
